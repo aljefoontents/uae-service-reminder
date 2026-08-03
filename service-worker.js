d0d961e392d1511a0a22a7c82887c47808a17cf6
Al Jefoon Tents <226182358+aljefoontents@users.noreply.github.com>
2026-08-03 13:56:17 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,131 @@
+/* =====================================================
+   REMINDO v1.3
+   Service Worker
+===================================================== */
 
+
+const CACHE_NAME = "remindo-v1";
+
+
+const FILES_TO_CACHE = [
+
+"./",
+
+"./index.html",
+
+"./style.css",
+
+"./script.js",
+
+"./manifest.json",
+
+"./icon-512.png",
+
+"./favicon.png"
+
+];
+
+
+
+
+// INSTALL
+
+self.addEventListener(
+"install",
+event=>{
+
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache=>{
+
+return cache.addAll(
+FILES_TO_CACHE
+);
+
+})
+
+);
+
+
+});
+
+
+
+
+
+
+
+// ACTIVATE
+
+self.addEventListener(
+"activate",
+event=>{
+
+
+event.waitUntil(
+
+caches.keys()
+
+.then(keys=>{
+
+
+return Promise.all(
+
+keys.map(key=>{
+
+
+if(key !== CACHE_NAME){
+
+return caches.delete(key);
+
+}
+
+
+})
+
+);
+
+
+})
+
+);
+
+
+});
+
+
+
+
+
+
+
+// FETCH
+
+self.addEventListener(
+"fetch",
+event=>{
+
+
+event.respondWith(
+
+
+caches.match(event.request)
+
+.then(response=>{
+
+
+return response ||
+
+fetch(event.request);
+
+
+})
+
+
+);
+
+
+});
